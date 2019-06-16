12c1c93b1fa1c33d7d787a03be2d611f6a61b81e
qq1071131216 <15222369963@163.com>
2019-06-16 22:47:50 +0800

    提交

--- project/src/main/java/cn/itcast/Test.java
+++ project/src/main/java/cn/itcast/Test.java
@@ -4,6 +4,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git第一次");
         System.out.println("git远程调用");
-	System.out.println("试一下");
     }
 }
