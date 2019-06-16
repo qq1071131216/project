a44d8fe74ea5e85d4fbe3b81a2fc65bccee148d2
qq1071131216 <15222369963@163.com>
2019-06-16 22:41:02 +0800

    试一下

--- project/src/main/java/cn/itcast/Test.java
+++ project/src/main/java/cn/itcast/Test.java
@@ -1,8 +1,9 @@
-package cn.itcast;
+﻿package cn.itcast;
 
 public class Test {
     public static void main(String[] args) {
         System.out.println("git第一次");
         System.out.println("git远程调用");
+	System.out.println("试一下");
     }
 }
